2923d3ec99a42812cd946df8f46d7d4ba6436397
tomaszjusko <tomekjusko@gmail.com>
2023-04-02 16:54:43 +0200

    jakas zmiana

--- src/pl/camp/it/git/App.java
+++ src/pl/camp/it/git/App.java
@@ -4,5 +4,6 @@ public class App {
     public static void main(String[] args) {
         System.out.println("Czesc");
         System.out.println("Pan jestes git");
+        System.out.println("cos");
     }
 }
